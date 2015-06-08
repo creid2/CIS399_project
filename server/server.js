9e86c71fb8c968cdfb72a2b567aa7ff1cc3938e3
Carl Reid <creid2@uoregon.edu>
2015-06-07 20:47:15 -0700

    Added timer and question display (pair programming with Carl and Matty)

--- server/server.js
+++ server/server.js
@@ -4,16 +4,32 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose'); // May need to change this
 
+var questionsLeft = []; // Keeps track of questions in the set
+
+var connectedClients = [];  // think about if this is needed
 
 io.on('connection', function(socket){									
-		io.emit('id', socket.id);
-		console.log('client connected!');
+    io.emit('id', socket.id);
+    connectedClients.push(socket.id);
+    console.log('client connected with socket id', socket.id);
+
+    // Send some random questions over to the client that just connected
+    // Not sure why this doesn't work.
+    getAnswerGrid('addition', socket.id);
+    //broadcastQuestion();
+});
+
+// Doesn't seem to work, but probably not a priority to figure out why.
+io.on('disconnect', function(socket){
+    console.log("client id", socket.id, "disconnected");
+    var i = connectedClients.indexOf(socket.id);
+    connectedClients.splice(i, 1); // remove the specified socket id
 });
 
 app.use(express.static(__dirname+"/../client/"));
 
-http.listen(3002,function(){
-		console.log('listening!');
+http.listen(3000,function(){
+    console.log('listening!');
 });
 
 // Temporarily using mongoose: this may change to nedb
@@ -23,9 +39,10 @@ mongoose.connect("mongodb://localhost/bingodatabase");
 // to have various effects on connect/disconnect, or database error.
 // For now, it just console.logs the existence of the event.
 mongoose.connection.on('connected', function() {
-  console.log("Connected to Mongoose.");
-  console.log("Adding a random math question...");
-  randomQuestion();
+    console.log("Connected to Mongoose.");
+    console.log("Adding a random math question...");
+    randomQuestion();
+    initGame('addition');
 });
 
 mongoose.connection.on('error',function (err) {
@@ -55,31 +72,91 @@ var User = mongoose.model("User", UserSchema);
 var Question = mongoose.model("Question", QuestionSchema);
 
 function randomQuestion() {
-  // Insert a single record into the database, as an example. This will run
-  // every time node starts up, so it will populate the database over time.
-  // This uses a random math question, so as to provide some variety.
+    // Insert a single record into the database, as an example. This will run
+    // every time node starts up, so it will populate the database over time.
+    // This uses a random math question, so as to provide some variety.
+    // Returns:
+    //     Nothing
 
-  // Get two random numbers from 0 to 9. 
-  var x = Math.floor(Math.random() * 10);
-  var y = Math.floor(Math.random() * 10);
-  var ans = x + y; // add them
+    // Get two random numbers from 0 to 9. 
+    var x = Math.floor(Math.random() * 10);
+    var y = Math.floor(Math.random() * 10);
+    var ans = x + y; // add them
 
-  // Make strings for the question and answer
-  var q = x.toString() + " + " + y.toString();
-  var a = (x + y).toString();
+    // Make strings for the question and answer
+    var q = x.toString() + " + " + y.toString();
+    var a = (x + y).toString();
 
-  // This try...catch block is because I was getting weird errors.
-  try {
-    var newQuestion = new Question({'question': q, 'answer': a, 'q_set':'addition'});
+    var newQuestion = new Question({'question':q, 'answer':a, 'q_set':'addition'});
     // Save, and console log the result of saving
     newQuestion.save(function (err, doc){ 
-        console.log( "New question result"
-             + JSON.stringify( err ) + " & " + JSON.stringify( doc));
+        console.log( "New question result: err=" + 
+             JSON.stringify( err ) + " & result=" + JSON.stringify( doc));
     });
-  } catch(e) {
-    console.log("Error trying to instantitate new question:");
-    console.log(e);
-  }
+}
 
+function getAnswerGrid(qSet, socket) {
+    // Returns a random 5x5 array of answers from the given question set. If
+    // the question set does not have enough questions in it to fill a 5x5 
+    // array, "Insufficient questions in set" will be appended to fill out the
+    // extra space.
+    // Returns:
+    //      5x5 array of strings
+
+    // Note: Mongo has no native equivalent to SQL's ORDER BY RAND()
+
+    // Searches for everything matching qSet, only selecting 'question' and 
+    // 'answer' fields (although _id is automatically included)
+    var ans = Question.find({'q_set':qSet}, 'question answer', function(err, res) {
+
+        if (err) {
+            console.log("Error:", err);
+        } else {
+            //console.log("Returned:\n", res);
+            var num = 0;
+            res.forEach(function getQuestion(cur) {
+                //console.log("Question:", cur.question, "\tAnswer:", cur.answer);
+                num++;
+            });
+
+            // Randomize the order of the array, Thanks to:
+            // http://jsfromhell.com/array/shuffle
+            for(var j, x, i = res.length; i; j = parseInt(Math.random() * i), x = res[--i], res[i] = res[j], res[j] = x);
+            // Now take the first 25 elements
+            if (res.length > 25) {
+                res = res.slice(0, 25);
+            } else {
+                while (res.length < 25) {
+                    res.push( {"question": "Insufficient questions",
+                                "answer" : "Insufficient questions"});
+                }
+            }
+            io.to(socket).emit("randomBoard", res);
+        }
+    });
+    //console.log("Logging variable 'ans':\n", ans);
+}
+
+function initGame(qSet) {
+    Question.find({'q_set':qSet}, 'question', function(err, res) {
+        if (err) {
+            console.log("Error in initGame:", err);
+            questionsLeft.push("Error getting a question");
+        } else {
+            for(var j, x, i = res.length; i; j = parseInt(Math.random() * i), x = res[--i], res[i] = res[j], res[j] = x);
+            questionsLeft = res;
+            nextQuestion();
+        }
+    });
+}
+
+function nextQuestion() {
+    //console.log("looking at global variable questionsLeft", questionsLeft);
+    questionsLeft.pop();
+    //broadcastQuestion();
+    if (questionsLeft.length > 0) {
+        io.emit("newQuestion", questionsLeft[questionsLeft.length - 1].question);
+        setInterval(function(){nextQuestion()}, 7000);
+    }
 }
 
